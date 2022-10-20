75b015a400f2a08a4ecdd3a9a24f367ffad2c13e
bei8 <bezi.zhong@gmail.com>
2022-10-20 17:56:31 +0700

    CRUD User

---
+++ app/Http/Controllers/Admin/UserController.php
@@ -0,0 +1,60 @@
+<?php
+
+namespace App\Http\Controllers\Admin;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Http\Request;
+use App\User;
+
+class UserController extends Controller
+{
+    public function index(Request $request)
+    {
+        $dataUser = User::all();
+                
+        if($request->ajax()){
+            return datatables()->of($dataUser)
+                ->addColumn('action', function($data){
+                        $button = '<a href="javascript:void(0)" data-toggle="tooltip" data-id="'.$data->id.'" data-original-title="Edit" class="edit btn btn-success btn-xs edit-post"><i class="fa fa-pen"></i></a>';
+                        $button .= '&nbsp;&nbsp;';
+                        $button .= '<button type="button" name="delete" id="'.$data->id.'" class="delete btn btn-danger btn-xs"><i class="fa fa-trash"></i></button>';
+                        return $button;
+                })
+                ->rawColumns(['action'])
+                ->addIndexColumn(true)
+                ->make(true);
+        }
+        return view('administrator.user.index');
+    }
+
+    public function store(Request $request)
+    {
+        $request->validate(
+            ['name' => 'required',],
+            ['name.required' => 'Anda belum menginputkan tahun',]
+        );
+
+        $post = User::updateOrCreate(['id' => $request->id],
+                [
+                    'name'       => $request->name,
+                    'email'   => $request->email,
+                    'password' => Hash::make($request['password']),
+                ]); 
+
+        return response()->json($post);
+    }
+
+    public function edit($id)
+    {
+        $where = array('id' => $id);
+        $post  = User::where($where)->first();     
+        return response()->json($post);
+    }
+
+    public function destroy($id)
+    {
+        $post = User::where('id',$id)->delete();     
+        return response()->json($post);
+    }
+}
---
+++ resources/views/administrator/user/index.blade.php
@@ -0,0 +1,248 @@
+@extends('layouts.backend')
+@section('title','All Users')
+
+@section('breadcrumbs')
+<div class="container">
+<nav aria-label="breadcrumb mb-0">
+    <ol class="breadcrumb breadcrumb-style2">
+      <li class="breadcrumb-item">
+        <a href="{{route('dashboard')}}">Home</a>
+      </li>
+      <li class="breadcrumb-item">
+        <a href="{{route('user.index')}}">@yield('title')</a>
+      </li>
+      <li class="breadcrumb-item active">Data</li>
+    </ol>
+</nav>
+</div>
+@endsection
+
+@section('content')
+
+<div class="container flex-grow-1">
+    <section id="basic-datatable">
+        <div class="row">
+            <div class="col-12">
+                <div class="card">
+                    <div class="card-body">
+                        <!-- MULAI TOMBOL TAMBAH -->
+                        <div class="mb-3">
+                            <a href="javascript:void(0)" class="btn btn-primary btn-md" id="tombol-tambah"><i class="fa fa-plus"></i> Add Data</a>
+                        </div>
+                        
+                        <!-- AKHIR TOMBOL -->
+                            <table class="table table-hover table-responsive" id="table_user">
+                              <thead>
+                                <tr>
+                                  <th>#</th>
+                                  <th>Name</th>
+                                  <th>Email</th>
+                                  <th>Actions</th>
+                                </tr>
+                              </thead>
+                            </table>
+                        </div>
+                    </div>
+
+                    <!-- MULAI MODAL FORM TAMBAH/EDIT-->
+                    <div class="modal fade" id="tambah-edit-modal" aria-hidden="true">
+                        <div class="modal-dialog ">
+                            <div class="modal-content">
+                                <div class="modal-header">
+                                    <h5 class="modal-title" id="modal-judul"></h5>
+                                    <button type="button" class="close" data-dismiss="modal">&times;</button>
+                                </div>
+                                <div class="modal-body">
+                                    <form id="form-tambah-edit" name="form-tambah-edit" class="form-horizontal">
+                                        <div class="row">
+                                            <div class="col-sm-12">
+
+                                                <input type="hidden" name="id" id="id">
+
+                                                <div class="mb-3">
+                                                    <label for="name" class="form-label">Username</label>
+                                                    <input type="text" class="form-control" id="name" name="name" value="" placeholder="John Doe" />
+                                                    <span class="text-danger" id="nameErrorMsg"></span>
+                                                </div>
+
+                                                <div class="mb-3">
+                                                    <label for="email" class="form-label">Email</label>
+                                                    <input type="email" class="form-control" id="email" name="email" value="" placeholder="johndoe@email.com" />
+                                                    <span class="text-danger" id="emailErrorMsg"></span>
+                                                </div>
+
+                                                <div class="mb-3">
+                                                    <label for="password" class="form-label">Password</label>
+                                                    <input type="password" class="form-control" id="password" name="password" value="" />
+                                                    <span class="text-danger" id="passwordErrorMsg"></span>
+                                                </div>
+
+                                            </div>
+
+                                            <div class="col-sm-offset-2 col-sm-12">
+                                                <hr class="mt-2">
+                                                <button type="submit" class="btn btn-primary btn-block float-sm-end" id="tombol-simpan" value="create">Save</button>
+                                            </div>
+                                        </div>
+
+                                    </form>
+                                </div>
+                                <div class="modal-footer">
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <!-- AKHIR MODAL -->
+                    
+                </div>
+            </div>
+        </div>
+    </section>
+</div>
+
+@endsection
+@section('script')
+
+<script>
+    $(document).ready(function () {
+        $.ajaxSetup({
+            headers: {
+                'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
+            }
+        });
+    });
+
+    // DATATABLE
+    $(document).ready(function () {
+        var table = $('#table_user').DataTable({
+            processing: true,
+            serverSide: true,
+            ajax: "{{ route('user.index') }}",
+            columns: [
+                {data: null,sortable:false,
+                    render: function (data, type, row, meta) {
+                    return meta.row + meta.settings._iDisplayStart + 1;
+                    }
+                }, 
+                {data: 'name',name: 'name'},
+                {data: 'email',name: 'email'},
+                {data: 'action',name: 'action'},
+            ]
+        });
+    });
+
+    //TOMBOL TAMBAH DATA
+    $('#tombol-tambah').click(function () {
+        $('#button-simpan').val("create-post");
+        $('#id').val('');
+        $('#form-tambah-edit').trigger("reset");
+        $('#modal-judul').html("Add new data");
+        $('#tambah-edit-modal').modal('show');
+    });
+
+    // TOMBOL TAMBAH
+    if ($("#form-tambah-edit").length > 0) {
+        $("#form-tambah-edit").validate({
+            submitHandler: function (form) {
+                var actionType = $('#tombol-simpan').val();
+                $('#tombol-simpan').html('Saving..');
+
+                $.ajax({
+                    data: $('#form-tambah-edit').serialize(), 
+                    url: "{{ route('user.store') }}",
+                    type: "POST",
+                    dataType: 'json',
+                    success: function (data) {
+                        $('#form-tambah-edit').trigger("reset");
+                        $('#tambah-edit-modal').modal('hide');
+                        $('#tombol-simpan').html('Save');
+                        var oTable = $('#table_user').dataTable();
+                        oTable.fnDraw(false);
+                        Swal.fire({
+                            title: 'Good job!',
+                            text: 'Data saved successfully!',
+                            type: 'success',
+                            customClass: {
+                            confirmButton: 'btn btn-primary'
+                            },
+                            buttonsStyling: false,
+                            timer: 2000
+                        })
+                    },
+                    error: function(response) {
+                        $('#tahunIDErrorMsg').text(response.responseJSON.errors.tahun);
+                        $('#tombol-simpan').html('Save');
+                        Swal.fire({
+                            title: 'Error!',
+                            text: 'Data failed to save!',
+                            type: 'error',
+                            customClass: {
+                            confirmButton: 'btn btn-primary'
+                            },
+                            buttonsStyling: false,
+                            timer: 2000
+                        })
+                    }
+                });
+            }
+        })
+    }
+
+    // EDIT DATA
+    $('body').on('click', '.edit-post', function () {
+        var data_id = $(this).data('id');
+        $.get('user/' + data_id + '/edit', function (data) {
+            $('#modal-judul').html("Edit data");
+            $('#tombol-simpan').val("edit-post");
+            $('#tambah-edit-modal').modal('show');
+              
+            $('#id').val(data.id);
+            $('#name').val(data.name);
+            $('#email').val(data.email);
+            $('#password').val(data.password);
+        })
+    });
+
+    // TOMBOL DELETE
+    $(document).on('click', '.delete', function () {
+        dataId = $(this).attr('id');
+        Swal.fire({
+            title: 'Are you sure?',
+            text: "It will be deleted permanently!",
+            type: 'warning',
+            showCancelButton: true,
+            confirmButtonColor: '#3085d6',
+            cancelButtonColor: '#d33',
+            confirmButtonText: 'Yes, delete it!',
+            showLoaderOnConfirm: true,
+            preConfirm: function() {
+                return new Promise(function(resolve) {
+                    $.ajax({
+                        url: "user/" + dataId,
+                        type: 'DELETE',
+                        data: {id:dataId},
+                        dataType: 'json'
+                    }).done(function(response) {
+                        Swal.fire({
+                            title: 'Deleted!',
+                            text: 'Your data has been deleted.',
+                            type: 'success',
+                            timer: 2000
+                        })
+                        $('#table_user').DataTable().ajax.reload(null, true);
+                    }).fail(function() {
+                        Swal.fire({
+                            title: 'Oops!',
+                            text: 'Something went wrong with ajax!',
+                            type: 'error',
+                            timer: 2000
+                        })
+                    });
+                });
+            },
+        });
+    });
+
+</script>
+
+@endsection
