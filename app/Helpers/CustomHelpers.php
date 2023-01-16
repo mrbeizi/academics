44343b8f717fdbbbc734b246ce45bf8ffcb83764
bei8 <bezi.zhong@gmail.com>
2023-01-16 15:59:27 +0700

    update: helper

--- app/Helpers/CustomHelpers.php
+++ app/Helpers/CustomHelpers.php
@@ -16,3 +16,37 @@
             return 'active';
         }
     }
+
+    function tanggal_indonesia($tgl, $tampil_hari=false){
+        $nama_hari=array("Minggu", "Senin", "Selasa", "Rabu", "Kamis", "Jum'at", "Sabtu");
+        $nama_bulan = array (
+                1 => "Januari", "Februari", "Maret", "April", "Mei", "Juni", "Juli", "Agustus",
+                "September", "Oktober", "November", "Desember");
+        $tahun=substr($tgl,0,4);
+        $bulan=$nama_bulan[(int)substr($tgl,5,2)];
+        $tanggal=substr($tgl,8,2);
+        $text="";
+        if ($tampil_hari) {
+            $urutan_hari=date('w', mktime(0,0,0, substr($tgl,5,2), $tanggal, $tahun));
+            $hari=$nama_hari[$urutan_hari];
+            $text .= $hari.", ";
+        }
+            $text .=$tanggal ." ". $bulan ." ". $tahun;
+        return $text;
+    }
+
+    if(!function_exists('currency_IDR'))
+    {
+        function currency_IDR($value)
+        {
+            return "Rp." .number_format($value,0,',','.');
+        }
+    }
+
+    if(!function_exists('currencyIDRToNumeric'))
+    {
+        function currencyIDRToNumeric($value)
+        {
+            return preg_replace('/\D/','', $value);
+        }
+    }
